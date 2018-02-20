be6973c5befd633495b64ede468b10d3a73983b6
Dan O'Neill <dev@jdoneill.com>
2018-02-19 20:20:57 -0800

    refactor API to use Account class

--- gradle/retrofit/src/main/java/com/jdoneill/retrofit/fulcrum/data/FulcrumApi.java
+++ gradle/retrofit/src/main/java/com/jdoneill/retrofit/fulcrum/data/FulcrumApi.java
@@ -1,7 +1,5 @@
 package com.jdoneill.retrofit.fulcrum.data;
 
-import java.util.HashMap;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
@@ -10,5 +8,5 @@ import retrofit2.http.Query;
 public interface FulcrumApi{
     @Headers({"Accept: application/json"})
     @GET("api/v2/users")
-    Call<HashMap<String, Object>> getAccount(@Query("token") String apikey);
+    Call<Account> getAccount(@Query("token") String apikey);
 }
