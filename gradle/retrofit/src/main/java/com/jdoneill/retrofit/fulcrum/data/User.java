8e739fa100c720449da8438b200a1132193ee4fa
Dan O'Neill <dev@jdoneill.com>
2018-02-19 20:19:53 -0800

    create data classes from json schema

---
+++ gradle/retrofit/src/main/java/com/jdoneill/retrofit/fulcrum/data/User.java
@@ -0,0 +1,100 @@
+
+package com.jdoneill.retrofit.fulcrum.data;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    @SerializedName("first_name")
+    @Expose
+    private String firstName;
+    @SerializedName("last_name")
+    @Expose
+    private String lastName;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("image_small")
+    @Expose
+    private String imageSmall;
+    @SerializedName("image_large")
+    @Expose
+    private String imageLarge;
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("contexts")
+    @Expose
+    private List<Context> contexts = null;
+    @SerializedName("access")
+    @Expose
+    private Access access;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImageSmall() {
+        return imageSmall;
+    }
+
+    public void setImageSmall(String imageSmall) {
+        this.imageSmall = imageSmall;
+    }
+
+    public String getImageLarge() {
+        return imageLarge;
+    }
+
+    public void setImageLarge(String imageLarge) {
+        this.imageLarge = imageLarge;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Context> getContexts() {
+        return contexts;
+    }
+
+    public void setContexts(List<Context> contexts) {
+        this.contexts = contexts;
+    }
+
+    public Access getAccess() {
+        return access;
+    }
+
+    public void setAccess(Access access) {
+        this.access = access;
+    }
+
+}
